c3006ac9cdae6390e4f532c82a2502444c4a3ef7
ecalvo92 <ecalvo@crnova.com>
2025-05-24 10:08:27 -0600

    Commit Inicial

---
+++ SProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
