3ed302cbb62e732e3b7978e1f750426122ce20f0
ecalvo92 <ecalvo@crnova.com>
2025-06-14 09:22:25 -0600

    Receso

---
+++ SApi/SApi/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Registro")]
+        public IActionResult Registro()
+        {
+
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+
+
+            return Ok();
+        }
+
+    }
+}
