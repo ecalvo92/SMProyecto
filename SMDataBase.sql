8f506813b1bb9c1dc8bc6de09ad4889ba98a2dce
ecalvo92 <ecalvo@crnova.com>
2025-07-19 10:47:47 -0600

    Inicio de cambio de contraseña y BD

--- SMDataBase.sql
+++ SMDataBase.sql
@@ -20,6 +20,16 @@ CREATE TABLE [dbo].[TError](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
+CREATE TABLE [dbo].[TRol](
+	[IdRol] [bigint] IDENTITY(1,1) NOT NULL,
+	[NombreRol] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_TRol] PRIMARY KEY CLUSTERED 
+(
+	[IdRol] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 CREATE TABLE [dbo].[TUsuario](
 	[IdUsuario] [bigint] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](255) NOT NULL,
@@ -27,6 +37,7 @@ CREATE TABLE [dbo].[TUsuario](
 	[Identificacion] [varchar](20) NOT NULL,
 	[Contrasenna] [varchar](255) NOT NULL,
 	[Estado] [bit] NOT NULL,
+	[IdRol] [bigint] NOT NULL,
  CONSTRAINT [PK_TUsuario] PRIMARY KEY CLUSTERED 
 (
 	[IdUsuario] ASC
@@ -40,12 +51,23 @@ INSERT [dbo].[TError] ([IdError], [Mensaje], [Origen], [FechaHora], [IdUsuario])
 GO
 INSERT [dbo].[TError] ([IdError], [Mensaje], [Origen], [FechaHora], [IdUsuario]) VALUES (2, N'Could not find stored procedure ''ValidarInicioSesion2''.', N'/api/Home/Index', CAST(N'2025-06-21T10:21:24.190' AS DateTime), 0)
 GO
+INSERT [dbo].[TError] ([IdError], [Mensaje], [Origen], [FechaHora], [IdUsuario]) VALUES (3, N'Could not find stored procedure ''ConsultaUsuario2''.', N'/api/Usuario/ConsultarUsuario', CAST(N'2025-07-19T10:20:03.233' AS DateTime), 1)
+GO
 SET IDENTITY_INSERT [dbo].[TError] OFF
+GO
+
+SET IDENTITY_INSERT [dbo].[TRol] ON 
+GO
+INSERT [dbo].[TRol] ([IdRol], [NombreRol]) VALUES (1, N'Usuario Regular')
+GO
+INSERT [dbo].[TRol] ([IdRol], [NombreRol]) VALUES (2, N'Usuario Administrador')
+GO
+SET IDENTITY_INSERT [dbo].[TRol] OFF
 GO
 
 SET IDENTITY_INSERT [dbo].[TUsuario] ON 
 GO
-INSERT [dbo].[TUsuario] ([IdUsuario], [Nombre], [CorreoElectronico], [Identificacion], [Contrasenna], [Estado]) VALUES (1, N'Eduardo Calvo Castillo', N'ecalvo90415@ufide.ac.cr', N'304590415', N'cMgQC+171mKu5Vsjm/JCwg==', 1)
+INSERT [dbo].[TUsuario] ([IdUsuario], [Nombre], [CorreoElectronico], [Identificacion], [Contrasenna], [Estado], [IdRol]) VALUES (1, N'JOSUE CAMPOS ACUÑA', N'jcampos40909@ufide.ac.cr', N'118940909', N'RRuOCbl+Uz4MRh1R56n7Nw==', 1, 2)
 GO
 SET IDENTITY_INSERT [dbo].[TUsuario] OFF
 GO
@@ -62,6 +84,12 @@ ALTER TABLE [dbo].[TUsuario] ADD  CONSTRAINT [uk_NombreUsuario] UNIQUE NONCLUSTE
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[TUsuario]  WITH CHECK ADD  CONSTRAINT [FK_TUsuario_TRol] FOREIGN KEY([IdRol])
+REFERENCES [dbo].[TRol] ([IdRol])
+GO
+ALTER TABLE [dbo].[TUsuario] CHECK CONSTRAINT [FK_TUsuario_TRol]
+GO
+
 CREATE PROCEDURE [dbo].[ActualizarContrasenna]
 	@IdUsuario bigint,
     @Contrasenna varchar(255)
@@ -71,6 +99,49 @@ BEGIN
 	UPDATE dbo.TUsuario
 	SET Contrasenna = @Contrasenna
 	WHERE IdUsuario = @IdUsuario
+
+END
+GO
+
+CREATE PROCEDURE [dbo].[ActualizarUsuario]
+	@IdUsuario bigint,
+    @Identificacion varchar(20),
+	@Nombre varchar(255),
+	@CorreoElectronico varchar(100)
+AS
+BEGIN
+
+	IF NOT EXISTS(SELECT 1 FROM dbo.TUsuario
+				  WHERE Identificacion = @Identificacion
+					AND	CorreoElectronico = @CorreoElectronico
+					AND IdUsuario != @IdUsuario)
+	BEGIN
+
+		UPDATE dbo.TUsuario
+		SET Identificacion = @Identificacion,
+			Nombre = @Nombre,
+			CorreoElectronico = @CorreoElectronico
+		WHERE IdUsuario = @IdUsuario
+		
+	END
+END
+GO
+
+CREATE PROCEDURE [dbo].[ConsultaUsuario]
+	@IdUsuario BIGINT
+AS
+BEGIN
+
+	SELECT	IdUsuario,
+			Nombre,
+			CorreoElectronico,
+			Identificacion,
+			Estado,
+			U.IdRol,
+			R.NombreRol
+	  FROM	dbo.TUsuario U
+	  INNER JOIN dbo.TRol R ON U.IdRol = R.IdRol
+	WHERE	IdUsuario = @IdUsuario
 
 END
 GO
@@ -102,8 +173,8 @@ BEGIN
 					OR	CorreoElectronico = @CorreoElectronico)
 	BEGIN
 
-		INSERT INTO dbo.TUsuario (Nombre,CorreoElectronico,Identificacion,Contrasenna,Estado)
-		VALUES (@Nombre, @CorreoElectronico, @Identificacion, @Contrasenna, @Estado)
+		INSERT INTO dbo.TUsuario (Nombre,CorreoElectronico,Identificacion,Contrasenna,Estado,IdRol)
+		VALUES (@Nombre, @CorreoElectronico, @Identificacion, @Contrasenna, @Estado,1)
 
 	END		
 END
@@ -133,8 +204,12 @@ BEGIN
 	SELECT	IdUsuario,
 			Nombre,
 			CorreoElectronico,
-			Identificacion
-	  FROM	dbo.TUsuario
+			Identificacion,
+			Estado,
+			U.IdRol,
+			R.NombreRol
+	  FROM	dbo.TUsuario U
+	  INNER JOIN dbo.TRol R ON U.IdRol = R.IdRol
 	WHERE	CorreoElectronico = @CorreoElectronico
 		AND Contrasenna = @Contrasenna
 
--- SProyecto/SProyecto/Controllers/UsuarioController.cs
+++ SProyecto/SProyecto/Controllers/UsuarioController.cs
@@ -20,7 +20,7 @@ namespace SProyecto.Controllers
             _utilitarios = utilitarios;
         }
 
-        #region Index
+        #region ActualizarPerfil
 
         [HttpGet]
         public IActionResult ActualizarPerfil()
@@ -62,6 +62,61 @@ namespace SProyecto.Controllers
                 if (resultado.IsSuccessStatusCode)
                 {
                     HttpContext.Session.SetString("Nombre", autenticacion?.Nombre!);                   
+                    return RedirectToAction("Principal", "Home");
+                }
+                else
+                {
+                    var respuesta = resultado.Content.ReadFromJsonAsync<RespuestaEstandar>().Result;
+                    ViewBag.Mensaje = respuesta!.Mensaje;
+                    return View();
+                }
+            }
+        }
+
+        #endregion
+
+        #region CambiarContrasenna
+
+        [HttpGet]
+        public IActionResult CambiarContrasenna()
+        {
+            using (var http = _http.CreateClient())
+            {
+                var IdUsuario = HttpContext.Session.GetString("IdUsuario");
+                http.BaseAddress = new Uri(_configuration.GetSection("Start:ApiUrl").Value!);
+
+                http.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWT"));
+                var resultado = http.GetAsync("api/Usuario/ConsultarUsuario?IdUsuario=" + IdUsuario).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var datos = resultado.Content.ReadFromJsonAsync<RespuestaEstandar<Autenticacion>>().Result;
+                    return View(datos!.Contenido);
+                }
+                else
+                {
+                    var respuesta = resultado.Content.ReadFromJsonAsync<RespuestaEstandar>().Result;
+                    ViewBag.Mensaje = respuesta!.Mensaje;
+                    return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CambiarContrasenna(Autenticacion autenticacion)
+        {
+            using (var http = _http.CreateClient())
+            {
+                var IdUsuario = HttpContext.Session.GetString("IdUsuario");
+                autenticacion.IdUsuario = long.Parse(IdUsuario!);
+
+                http.BaseAddress = new Uri(_configuration.GetSection("Start:ApiUrl").Value!);
+                http.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWT"));
+                var resultado = http.PutAsJsonAsync("api/Usuario/ActualizarUsuario", autenticacion).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    HttpContext.Session.SetString("Nombre", autenticacion?.Nombre!);
                     return RedirectToAction("Principal", "Home");
                 }
                 else
