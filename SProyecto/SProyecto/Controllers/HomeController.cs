f814ea465258716cf0618147ce503a44b2bb8439
ecalvo92 <ecalvo@crnova.com>
2025-08-02 17:53:48 -0600

    Tabla de productos principal

--- SProyecto/SProyecto/Controllers/HomeController.cs
+++ SProyecto/SProyecto/Controllers/HomeController.cs
@@ -134,7 +134,24 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Principal()
     {
-        return View();
+        using (var http = _http.CreateClient())
+        {
+            http.BaseAddress = new Uri(_configuration.GetSection("Start:ApiUrl").Value!);
+            http.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWT"));
+            var resultado = http.GetAsync("api/Productos/ConsultarProductos").Result;
+
+            if (resultado.IsSuccessStatusCode)
+            {
+                var datos = resultado.Content.ReadFromJsonAsync<RespuestaEstandar<List<Producto>>>().Result;
+                return View(datos?.Contenido!);
+            }
+            else
+            {
+                var respuesta = resultado.Content.ReadFromJsonAsync<RespuestaEstandar>().Result;
+                ViewBag.Mensaje = respuesta?.Mensaje;
+                return View();
+            }
+        }
     }
 
 }
